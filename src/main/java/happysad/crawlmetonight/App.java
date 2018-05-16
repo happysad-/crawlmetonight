3b29ebed918ea5f2781db38067a54b30e0336e99
happysad- <bucinskas.martin@gmail.com>
2018-05-16 14:10:36 +0100

    Initial Commit.

--- src/main/java/happysad/crawlmetonight/App.java
+++ src/main/java/happysad/crawlmetonight/App.java
@@ -1,13 +1,15 @@
 package happysad.crawlmetonight;
 
 /**
- * Hello world!
+ * Initial entry point.
+ * 
+ * @author Martin Bucinskas
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "crawlmetonight" );
     }
 }
